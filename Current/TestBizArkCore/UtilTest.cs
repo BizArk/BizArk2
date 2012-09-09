fc4c3b1dae5acf09e6a780848edc9e2c0e106cba
borismod_cp <SND\borismod_cp>
2012-09-09 22:15:46 +0000

    Fixing RemoteTimeTest

--- Current/TestBizArkCore/UtilTest.cs
+++ Current/TestBizArkCore/UtilTest.cs
@@ -1,43 +1,40 @@
 ﻿using System;
 using System.Threading;
-using BizArk.Core.Extensions.FormatExt;
 using BizArk.Core.Util;
 using NUnit.Framework;
 
 namespace TestBizArkCore
 {
-    
-    
-    [TestFixture]
-    public class UtilTest
-    {
-        
-        [Test]
-        public void RemoteTimeTest()
-        {
-            var remote = new RemoteDateTime(DateTime.Now);
-            AssertEx.AreClose(DateTime.Now, remote.Now, TimeSpan.FromMilliseconds(1));
-            Thread.Sleep(TimeSpan.FromSeconds(5));
-            AssertEx.AreClose(DateTime.Now, remote.Now, TimeSpan.FromMilliseconds(1));
-        }
+	[TestFixture]
+	public class UtilTest
+	{
+		[Test]
+		public void RangeIncludesTest()
+		{
+			var range1 = new Range<long>(0, 5);
+			Assert.IsTrue(range1.Includes(3));
+			Assert.IsTrue(range1.Includes(0));
+			Assert.IsTrue(range1.Includes(5));
+			Assert.IsFalse(range1.Includes(-1));
+			Assert.IsFalse(range1.Includes(6));
 
-        [Test]
-        public void RangeIncludesTest()
-        {
-            var range1 = new Range<long>(0, 5);
-            Assert.IsTrue(range1.Includes(3));
-            Assert.IsTrue(range1.Includes(0));
-            Assert.IsTrue(range1.Includes(5));
-            Assert.IsFalse(range1.Includes(-1));
-            Assert.IsFalse(range1.Includes(6));
+			var range2 = new Range<string>("b", "f");
+			Assert.IsTrue(range2.Includes("d"));
+			Assert.IsTrue(range2.Includes("b"));
+			Assert.IsTrue(range2.Includes("f"));
+			Assert.IsFalse(range2.Includes("a"));
+			Assert.IsFalse(range2.Includes("g"));
+		}
 
-            var range2 = new Range<string>("b", "f");
-            Assert.IsTrue(range2.Includes("d"));
-            Assert.IsTrue(range2.Includes("b"));
-            Assert.IsTrue(range2.Includes("f"));
-            Assert.IsFalse(range2.Includes("a"));
-            Assert.IsFalse(range2.Includes("g"));
-        }
-
-    }
+		[Test]
+		public void RemoteTimeTest()
+		{
+			var remote = new RemoteDateTime(DateTime.Now);
+			AssertEx.AreClose(DateTime.Now, remote.Now, TimeSpan.FromMilliseconds(1));
+			Thread.Sleep(TimeSpan.FromSeconds(5));
+			DateTime now = DateTime.Now;
+			DateTime remoteNow = remote.Now;
+			AssertEx.AreClose(now, remoteNow, TimeSpan.FromMilliseconds(1));
+		}
+	}
 }
