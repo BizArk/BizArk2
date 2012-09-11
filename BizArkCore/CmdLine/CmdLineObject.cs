04ed1532ee5f6ed1726a10ec19ddb6b2ef6e666f
Boris Modylevsky <borismod@gmail.com>
2012-09-11 12:46:56 +0000

    Fixes for items: 7513: Exception is thrown when class derived from CmdLineObject is executed from Team City 7522: Set ExitCode which is different than 0 on exception

--- BizArkCore/CmdLine/CmdLineObject.cs
+++ BizArkCore/CmdLine/CmdLineObject.cs
@@ -16,6 +16,114 @@ using BizArk.Core.Web;
 
 namespace BizArk.Core.CmdLine
 {
+	public interface ICmdLineObject
+	{
+		/// <summary>
+		/// Gets the options used for the handling the command-line object.
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		[Browsable(false)]
+		CmdLineOptions Options { get; }
+
+		/// <summary>
+		/// Gets or sets a value that determines if help should be displayed.
+		/// </summary>
+		[CmdLineArg(Alias = "?", ShowInUsage = DefaultBoolean.True)]
+		[Description("Displays command-line usage information.")]
+		[Browsable(false)]
+		[DefaultValue(false)]
+		bool Help { get; set; }
+
+		/// <summary>
+		/// Gets the list of command-line properties.
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		[Browsable(false)]
+		CmdLinePropertyList Properties { get; }
+
+		/// <summary>
+		/// Gets the error text for the command-line object.
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		[Browsable(false)]
+		string ErrorText { get; }
+
+		/// <summary>
+		/// Gets a value that determines if the CmdLineObject is ready to use.
+		/// </summary>
+		bool IsInitialized { get; }
+
+		/// <summary>
+		/// Gets the default properties for the command-line.
+		/// </summary>
+		CmdLineProperty[] DefaultProperties { get; }
+
+		/// <summary>
+		/// Initializes the CmdLineObject.
+		/// </summary>
+		void Initialize();
+
+		/// <summary>
+		/// Initializes the command-line object, but does not populate it.
+		/// </summary>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		void InitializeEmpty();
+
+		/// <summary>
+		/// Initializes the command-line args based on a query string. Used for 
+		/// </summary>
+		/// <param name="queryStr"></param>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		void InitializeFromQueryString(string queryStr);
+
+		/// <summary>
+		/// Initializes the object with the given arguments.
+		/// </summary>
+		/// <param name="args">The command-line args. Make sure to shrink the array if the first element contains the path to the application (as in Environment.GetCommandLineArgs()) or the default parameter won't get set correctly.</param>
+		/// <example>
+		/// using BizArk.Core.ArrayExt;
+		/// var args = Environment.GetCommandLineArgs().Shrink(1);
+		/// </example>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		void InitializeFromCmdLine(params string[] args);
+
+		/// <summary>
+		/// Makes sure the command-line object is valid. 
+		/// </summary>
+		/// <returns></returns>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		[Browsable(false)]
+		bool IsValid();
+
+		/// <summary>
+		/// Gets the full description for the command-line arguments.
+		/// </summary>
+		/// <param name="maxWidth">Determines the number of characters per line. Set this to Console.Width.</param>
+		/// <returns></returns>
+		string GetHelpText(int maxWidth);
+
+		/// <summary>
+		/// Saves the settings to an xml file.
+		/// </summary>
+		/// <param name="path"></param>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		void SaveToXml(string path);
+
+		/// <summary>
+		/// Restores the settings from an xml file.
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns>True if the settings are restored from the file.</returns>
+		[EditorBrowsable(EditorBrowsableState.Advanced)]
+		bool RestoreFromXml(string path);
+
+		/// <summary>
+		/// Gets the usage for this command-line object.
+		/// </summary>
+		/// <returns></returns>
+		string ToString();
+	}
+
 	/// <summary>
 	/// Represents an object that can be initialized via command-line arguments.
 	/// </summary>
@@ -37,7 +145,7 @@ namespace BizArk.Core.CmdLine
 	/// CmdLineArgAttribute and the description can be set by applying the
 	/// System.ComponentModel.DescriptionAttribute to the property.</para>
 	/// </remarks>
-	public abstract class CmdLineObject
+	public abstract class CmdLineObject : ICmdLineObject
 	{
 		#region Initialization and Destruction
 
