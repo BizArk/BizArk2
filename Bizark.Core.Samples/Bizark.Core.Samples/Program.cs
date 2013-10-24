b82d307ff0a5c83d53ac0a159a6912f081651051
Boris Modylevsky <borismod@gmail.com>
2013-10-24 06:16:09 +0000

    samples project files added

---
+++ Bizark.Core.Samples/Bizark.Core.Samples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BizArk.Core;
+using BizArk.Core.CmdLine;
+
+namespace Bizark.Core.Samples
+{
+    static class Program
+    {
+        private static void Main(string[] args)
+        {
+            ConsoleApplication.RunProgram<SimpleCommandLineArguments>(RunMain);
+        }
+
+        private static void RunMain(SimpleCommandLineArguments args)
+        {
+            Console.WriteLine("Name is {0}, Age is {1}", args.Name, args.Age);
+            Console.ReadLine();
+        }
+
+        private class SimpleCommandLineArguments : CmdLineObject
+        {
+            [CmdLineArg(ShowInUsage = DefaultBoolean.True)]
+            public string Name { get; set; }
+
+            [CmdLineArg(ShowInUsage = DefaultBoolean.True)]
+            public int Age { get; set; }
+        }
+    }
+}
